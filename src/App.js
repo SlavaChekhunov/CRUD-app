6482334e8fa2b2dc25308761b4bd89698502a694
SlavaChekhunov <slava97@my.yorku.ca>
2022-11-28 19:16:44 -0500

    basic crud todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+//capture whatever the user is typing in the input field and display it✔
+//now once the button is clicked save the values in the array  ✔
+//display what is in the array by looping with .map ✔
+//once clicked you want to delete the item from that array. 
+
 function App() {
+  const [task, setTask] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleClick = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const handleText = () => {
+    const newTask = [...task, inputValue];
+    setTask(newTask);
+  }
+
+  const handleDelete = () => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="input" onChange={handleClick}/>
+      <button className="btn" onClick={handleText}>
+        Send
+      </button>
+      {task.map(item =>{
+        return (
+          <>
+          <h1>{item}</h1>
+          <button onClick={handleDelete}>X</button>
+          </>
+        )
+      })}
     </div>
   );
 }
